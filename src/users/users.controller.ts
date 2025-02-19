50b7b6055a9ea8eacf7be368ff5cff89f819fa7b
Ammar <ammar@ovasave.com>
2025-02-19 09:21:37 +0400

    connecting user repository to user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    return this.userService.create({
+      email: body.email,
+      password: body.password,
+    });
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create({ email, password }: { email: string; password: string }) {
+    const user = await this.userRepository.save({ email, password });
+    return { user };
+  }
+}
