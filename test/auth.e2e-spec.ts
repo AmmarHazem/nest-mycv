b5f03accfd7dfc3107a85dc31454b9a49b286dd8
Ammar <ammar@ovasave.com>
2025-03-11 23:28:56 +0400

    trying to write e2e test for auth module

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from './../src/app.module';
+import * as request from 'supertest';
+
+describe('Authentication', () => {
+  let app: INestApplication;
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+  it('handles sign up request', async () => {
+    const email = 'asjdfhkjd@email.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: email, password: '123456' });
+    expect(res.status).toEqual(201);
+    expect(res.body.id).toBeDefined();
+    expect(res.body.email).toEqual(email);
+  });
+});
