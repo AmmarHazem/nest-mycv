03536681a38cbe6e4d29787927078f820804f4fc
Ammar <ammar@ovasave.com>
2025-03-13 00:17:17 +0400

    add validation pipe as an app module provider

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -13,7 +13,7 @@ describe('Authentication', () => {
     await app.init();
   });
   it('handles sign up request', async () => {
-    const email = 'asjdfhkjd@email.com';
+    const email = 'asjdfhkjsd@email.com';
     const res = await request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email: email, password: '123456' });
