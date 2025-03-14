8031c482bc252e26fe550bc33f052e024843ba81
Ammar <ammar@ovasave.com>
2025-03-14 22:56:58 +0400

    configure cookie middleware global

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-import cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({ keys: ['salkfkjsahdfkadbf'] }));
+  app.use();
   await app.listen(3000);
 }
 bootstrap();
